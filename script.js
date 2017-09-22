4abc54e2d2d16b0b631c0c3bd77c3019fdafb1ee
Norris Kwan <norris.kwan@gmail.com>
2017-09-22 16:02:42 -0700

    switch

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const clock = document.getElementById('clock');
 
-    clock.innerHTML = "test";
-
-    const time = new Date;
-    let hours = time.getHours() + 1;
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-    console.log(clock);
-    console.log();
+    let counter = setInterval(() => {
+        let time = new Date;
+        let hours = (time.getHours() % 12);
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        let ap = time.getHours();
+        clock.innerHTML =
+            `${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)} ${ampm(ap)}`;
+    }, 1000);
 });
+
+
+function formatTime(num) {
+    return (num < 10) ? `0${num}` : `${num}`;
+}
+
+function ampm(num) {
+    return (num > 12) ? 'pm' : 'am';
+}
